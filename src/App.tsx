5c6390f546c10a20ac8713fc83f588235e88411c
Jabir Jaleel <jabir@maxstacklabs.com>
2025-03-02 15:09:29 +0530

    initialize project with Vite, React, and TypeScript setup; add utility functions and components

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import FileExplorerHero from "./file-explorer";
+import HackerMode from "./hacker-mode";
+import Hero from "./hero";
+import Home from "./inspired";
+
+function App() {
+  return (
+    <>
+      <FileExplorerHero />
+      <Hero />
+      <HackerMode />
+      <Home />
+    </>
+  );
+}
+
+export default App;
